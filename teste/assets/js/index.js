a11ac8cbeb21c5b3840f345bc0bfbf12d7c1a871
Gustavo Andrade <gugufonceca.2004@gmail.com>
2025-11-17 13:28:00 -0300

    Remove original code comment from index.js

--- teste/assets/js/index.js
+++ teste/assets/js/index.js
@@ -1,4 +1,3 @@
-// --- SEU CÓDIGO ORIGINAL ---
 const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
 const tarefas = document.querySelector('.tarefas');
